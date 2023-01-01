ae78188f49d98b4d49ba3646bb2a0c5da27f063a
Arif Ebrahim <74261924+ArifEbrahim@users.noreply.github.com>
2023-01-01 13:59:52 +0000

    refactor tests

--- src/__tests__/unit/account.test.ts
+++ src/__tests__/unit/account.test.ts
@@ -1,28 +1,34 @@
 import Account from '../../account'
 
 describe('Account', () => {
+  let account: Account
+
+  beforeEach(() => {
+    account = new Account()
+  })
+
   test('should have a method that returns the balance', () => {
-    const account = new Account()
     expect(account).toHaveProperty('getBalance')
   })
 
   test('should start with a balance of 0', () => {
-    const account = new Account()
     expect(account.getBalance()).toBe(0)
   })
 
-  test('should allow users to make a depost', () => {
-    const account = new Account()
-    expect(account).toHaveProperty('deposit')
-    account.deposit(50)
-    expect(account.getBalance()).toBe(50)
+  describe('Deposit', () => {
+    test('should allow users to make a depost', () => {
+      expect(account).toHaveProperty('deposit')
+      account.deposit(50)
+      expect(account.getBalance()).toBe(50)
+    })
   })
 
-  test('should allow users to make a withdrawl', () => {
-    const account = new Account()
-    expect(account).toHaveProperty('withdraw')
-    account.deposit(100)
-    account.withdraw(25)
-    expect(account.getBalance()).toBe(75)
+  describe('Withdraw', () => {
+    test('should allow users to make a withdrawl', () => {
+      expect(account).toHaveProperty('withdraw')
+      account.deposit(100)
+      account.withdraw(25)
+      expect(account.getBalance()).toBe(75)
+    })
   })
 })
