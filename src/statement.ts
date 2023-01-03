d28fc59f224c40bd6e7527b5256a738a0178926e
Arif Ebrahim <74261924+ArifEbrahim@users.noreply.github.com>
2023-01-03 04:29:24 +0000

    refactor print method in statement

--- src/statement.ts
+++ src/statement.ts
@@ -1,5 +1,11 @@
 export default class Statement {
-  print(){
-    return 'date || credit || debit || balance'
+  private HEADER: string
+
+  constructor() {
+    this.HEADER = 'date || credit || debit || balance'
+  }
+
+  print() {
+    return this.HEADER
   }
 }
